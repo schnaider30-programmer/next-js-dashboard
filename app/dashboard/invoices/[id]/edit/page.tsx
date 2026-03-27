97e6a4ea321a0d38fd4d3e8994c8c0d10cafa767
schnaider30-programmer <sjeanlouis@byupathway.edu>
2026-03-27 13:26:26 -0400

    added metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,7 +3,11 @@ import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { notFound } from "next/navigation";
 
- 
+ import { Metadata } from "next";
+
+ export const metadata: Metadata = {
+   title: "Edit",
+ };
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
     const id = params.id;
