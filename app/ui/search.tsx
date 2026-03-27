2f010256e4cd631f548c1b3eb53762cbafa58cc1
schnaider30-programmer <sjeanlouis@byupathway.edu>
2026-03-27 08:01:22 -0400

    chapter ten (10) - complete

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,24 +2,23 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const pathname = usePathname()
-    const { replace } = useRouter();
-
-
-  function handleSearch(term: string) {
-    console.log(term);
-    console.log(searchParams);
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set("page", "1")
     if (term) {
-      params.set("query", term)
+      params.set("query", term);
     } else {
-      params.delete("query")
+      params.delete("query");
     }
-    replace(`${pathname}?${params.toString()}`)
-  }
+    replace(`${pathname}?${params.toString()}`);
+  }, 3000);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -31,6 +30,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
